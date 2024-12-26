bd6cee7aa14a49dc704892607e32d5ce9ae6bd22
jugem <1004traitor@gmail.com>
2024-12-26 22:31:45 +0900

    fix

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,5 @@
+import Sidebar from "@/components/Sidebar";
+
 export default async function Home() {
 
   // const response = await fetch('http://backend/api/sample');
@@ -6,11 +8,7 @@ export default async function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <nav className="bg-gray-800 text-white p-4">Navigation</nav>
-      <div className="flex-1 grid grid-cols-12 gap-4">
-        <aside className="bg-gray-200 p-4 col-span-2">Left Sidebar</aside>
-        <main className="bg-white p-4 col-span-8">Main Content</main>
-        <aside className="bg-gray-200 p-4 col-span-2">Right Sidebar</aside>
-      </div>
+        <Sidebar />
       <footer className="bg-gray-800 text-white p-4">Footer</footer>
     </div>
   );
