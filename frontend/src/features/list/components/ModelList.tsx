150ae356e332212d56ba0522e2a660dced7acc16
shimaoka hitoshi <shimaoka1203@gmail.com>
2025-02-19 22:42:12 +0900

    一覧ページのリンク範囲を全体に変更

--- frontend/src/features/list/components/ModelList.tsx
+++ frontend/src/features/list/components/ModelList.tsx
@@ -1,25 +1,22 @@
-import type { FC } from 'react';
-import { ModelCard } from './ModelCard';
-import { fetchFilteredModels } from '@/services/list';
+import { fetchFilteredModels } from "@/services/list";
+import type { FC } from "react";
+import { ModelCard } from "./ModelCard";
 
 type Props = {
-    query: string;
-    currentPage: number;
+	query: string;
+	currentPage: number;
 };
 
 export const ModelList: FC<Props> = async (props: Props) => {
-    const models = await fetchFilteredModels(props.query, props.currentPage);
-    return (
-        <>
-            {/* 画面幅でレイアウトを変える */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-                {models.map((model) => (
-                    <ModelCard
-                        key={model.id}
-                        model={model}
-                    />
-                ))}
-            </div>
-        </>
-    );
-}
+	const models = await fetchFilteredModels(props.query, props.currentPage);
+	return (
+		<>
+			{/* 画面幅でレイアウトを変える */}
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
+				{models.map((model) => (
+					<ModelCard key={model.id} model={model} />
+				))}
+			</div>
+		</>
+	);
+};
